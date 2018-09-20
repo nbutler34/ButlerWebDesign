037d9aa4e4d7ddcc3d34210630aa8c1d41a430ba
Nicholas <nicolibonsai@gmail.com>
2018-09-20 01:03:11 -0400

    SingleServe Complete

---
+++ singleServeWebsite/scripts/main.js
@@ -0,0 +1,8 @@
+function playVid() {
+    var x = document.getElementById("hideMe");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
