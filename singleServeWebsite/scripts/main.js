422d1165bcdbc6a68ba4ab1cd35ccf847bb5a569
Nicholas <nicolibonsai@gmail.com>
2018-09-20 01:42:52 -0400

    Logic Change 1

--- singleServeWebsite/scripts/main.js
+++ singleServeWebsite/scripts/main.js
@@ -1,9 +1,16 @@
 function playVid() {
     var x = document.getElementById("hideMe");
-    if (x.style.display === "block") {
+    if (x.style.display === "block" && x.paused) {
         x.style.display = "none";
     } else {
         x.style.display = "block";
         x.play();
+        setTimeout(hideVid, 10750);
     }
+}
+
+function hideVid() {
+	var x = document.getElementById("hideMe");
+	x.pause();
+	x.style.display = "none";
 }
