b858a597fa37607c406f370c63452cdf5fbd4d1b
Nicholas <nicolibonsai@gmail.com>
2018-12-06 18:54:40 -0500

    nav changes

--- projects/P4/scripts/main.js
+++ projects/P4/scripts/main.js
@@ -1,8 +1,27 @@
-$(window).on('scroll', function() {
-	if($(window).scrollTop()) {
-		$('nav').addClass('show');
-	}
-	else {
-		$('nav').removeClass('show');
-	}
+$(document).ready(function() {
+	var distance = $('nav').offset(),
+	$window = $(window);
+
+	console.log(distance.top);
+
+	$(window).on('scroll', function() {
+		if( ($window.scrollTop() >= distance.top) && ( $('nav').hasClass('show') )) {
+			// console.log('alteration prevented');
+			return false;
+		}
+		else if( $window.scrollTop() <= distance.top && !( $('nav').hasClass('show') )) {
+			// console.log('alteration prevented');
+			return false;
+		}
+
+		if( ($window.scrollTop() >= distance.top) && !( $('nav').hasClass('show') )) {
+			$('nav').addClass('show');
+			// console.log('show added');
+		}
+		else if( $window.scrollTop() <= distance.top && ( $('nav').hasClass('show') )) {
+			$('nav').removeClass('show');
+			// console.log('show removed')
+		}
+	});
+
 })
